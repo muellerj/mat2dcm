2fdf9d6bb0a426af4d947625d6c05887b7efef5e
Jonas Mueller <jonas@muellers.one>
2015-11-02 14:46:09 +0100

    Bugfix expect dimensions in specs

--- spec/utility_functions_spec.m
+++ spec/utility_functions_spec.m
@@ -15,7 +15,7 @@ expect(K_SOME_SCALAR == 1.3);
 expect(all(size(K_SOME_VECTOR) == [1 3]));
 expect(all(K_SOME_VECTOR == [1.0 2.0 3.0]));
 expect(all(size(K_SOME_MATRIX) == [2 2]));
-expect(all(K_SOME_MATRIX == [1 2; 3 4]));
+expect(all(all(K_SOME_MATRIX == [1 2; 3 4])));
 
 % Stuetzstellenverteilung
 expect(isfield(K_SOME_SSV, 'x'));
@@ -33,5 +33,5 @@ expect(isfield(K_SOME_KF, 'y'));
 expect(isfield(K_SOME_KF, 'y'));
 expect(all(K_SOME_KF.x == [0 1]));
 expect(all(K_SOME_KF.y == [1 2 3 ]));
-expect(all(K_SOME_KF.z == [10 20 30; 40 50 60]));
+expect(all(all(K_SOME_KF.z == [10 20 30; 40 50 60])));
 
